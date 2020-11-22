0be0d74e01872b63709f981ef2c700ce1fc5cd26
Cian John <topjohncian@gmail.com>
2020-11-22 18:30:36 +0800

    upload code

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	model "github.com/HFO4/cloudreve/models"
 	"github.com/HFO4/cloudreve/pkg/conf"
 	"github.com/HFO4/cloudreve/pkg/util"
@@ -11,25 +10,26 @@ import (
 
 var isPro bool
 
-func init()  {
-	flag.BoolVar(&isPro, "pro", false, "是否为pro版本")
-	flag.Parse()
+func init() {
+	if os.Args[3] == "-pro" {
+		isPro = true
+	}
 }
 
-func main(){
+func main() {
 	conf.Init(util.RelativePath("conf.ini"))
 	model.Init()
-	newPolicyID,err := strconv.ParseUint(os.Args[2],10,0)
-	if err != nil{
-		util.Log().Panic("发生错误: %s",err)
+	newPolicyID, err := strconv.ParseUint(os.Args[2], 10, 0)
+	if err != nil {
+		util.Log().Panic("发生错误: %s", err)
 	}
 	util.Log().Info("开始转换")
 	model.DB.Model(&model.File{}).
-		Where("policy_id = ?",os.Args[1]).
-		Updates(model.File{PolicyID:uint(newPolicyID)})
+		Where("policy_id = ?", os.Args[1]).
+		Updates(model.File{PolicyID: uint(newPolicyID)})
 	if isPro {
 		model.DB.Model(&model.Folder{}).
-			Where("policy_id = ?",os.Args[1]).
-			Update("policy_id",newPolicyID)
+			Where("policy_id = ?", os.Args[1]).
+			Update("policy_id", newPolicyID)
 	}
 }
